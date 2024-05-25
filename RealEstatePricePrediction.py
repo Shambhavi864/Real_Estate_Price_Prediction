ad915eeee4f04591bf2cd360d0fb81165c23dca3
Shambhavi864 <95189537+Shambhavi864@users.noreply.github.com>
2024-05-25 17:53:51 +0530

    Update RealEstatePricePrediction.py

--- RealEstatePricePrediction.py
+++ RealEstatePricePrediction.py
@@ -104,3 +104,58 @@ matplotlib.rcParams["figure.figsize"] = (20, 10)
 df9=df8[df8.bath<df8.bhk+2]
 
 df10= df9.drop(['size', 'price_per_sqrft'], axis = 'columns')
+
+# MODEL BUILDING
+
+dummies = pd.get_dummies(df10.location)
+
+df11 = pd.concat([df10,dummies.drop('other', axis='columns')], axis='columns')
+df12 = df11.drop('location', axis = 'columns')
+
+X = df12.drop('price', axis = 'columns')
+y = df12.price
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 10)
+lr_clf = LinearRegression()
+# Training the model
+lr_clf.fit(X_train, y_train)
+# Testing the score of the model
+lr_clf.score(X_test, y_test)
+
+#Score --> 84.522%
+
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import cross_val_score
+
+cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state = 0)
+
+cross_val_score(LinearRegression(), X, y, cv = cv)
+
+# Price Prediction
+
+def predict_price(location, sqft, bath, bhk):
+    loc_index = np.where(X.columns == location)[0][0]
+
+    x = np.zeros(len(X.columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+    return lr_clf.predict([x])[0]
+
+#importing a picle file
+import pickle
+with open('banglore_home_price_model.pickle', 'wb') as f:
+    pickle.dump(lr_clf, f)
+
+
+#importing json file
+import json
+columns = {
+    'data_columns': [col.lower() for col in X. columns]
+}
+with open("columns.json", "w") as f:
+    f.write(json.dumps (columns))
